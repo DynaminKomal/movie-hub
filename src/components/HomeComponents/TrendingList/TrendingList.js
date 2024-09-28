fad61ab9c05e3ab965315108e73c76cc87c455a5
Komal <91592874+DynaminKomal@users.noreply.github.com>
2024-09-28 14:40:57 +0530

    trending list update

--- src/components/HomeComponents/TrendingList/TrendingList.js
+++ src/components/HomeComponents/TrendingList/TrendingList.js
@@ -9,45 +9,12 @@ import rightIcon from '../../../assets/next-icon.svg';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import globalStyle from '../../../styles/globalStyle.module.scss';
-import Button from '../../HOC/Button/Button';
-import playIcon from '../../../assets/playIcon.svg';
-import saveIcon from '../../../assets/bookmark.svg';
-
-const Tooltip = ({ tooltipData, position }) => {
-    return ReactDOM.createPortal(
-        <div className={styles.tooltip} style={{ left: position.x, top: position.y }}>
-            <video autoPlay className={styles.videoBackgroundHolder}>
-                <source src={tooltipData.video_url} type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
-            <div className={styles.videoMeta}>
-                <h1 className={`${globalStyle.uppercase} ${styles.title}`}>{tooltipData.title} - {tooltipData.id}</h1>
-                <div className={styles.metaBox}>
-                    <span>{tooltipData.year}</span>
-                    <ul>
-                        <li>{tooltipData.timing}</li>
-                        <li>TV-MA</li>
-                    </ul>
-                    <div className={`${globalStyle.uppercase} ${styles.genre}`}>{tooltipData.genre}</div>
-                </div>
-                <div className={styles.buttonBox}>
-                    <Button name="Play Now" image_icon={playIcon} image_position="right" image_name="play icon" />
-                    <Button name="Watch Later" image_icon={saveIcon} image_position="right" image_name="Bookmark icon" />
-                </div>
-            </div>
-        </div>,
-        document.body
-    );
-};
 
 const TrendingList = (props) => {
     const { name } = props;
 
     const [currentIndex, setCurrentIndex] = useState(0);
     const itemsPerPage = 6;
-    const [tooltipData, setTooltipData] = useState(null);
-    const [tooltipPosition, setTooltipPosition] = useState({ x: 0, y: 0 });
 
     const data = [
         { id: "1", image_url: johnImage, title: "John Wick 1", year: "2023", timing: "1hr 25m", genre: "Action", video_url: john_video },
@@ -97,19 +64,6 @@ const TrendingList = (props) => {
         beforeChange: handleBeforeChange,
     };
 
-    const handleMouseEnter = (item, event) => {
-        const rect = event.currentTarget.getBoundingClientRect();
-        setTooltipData(item);
-        setTooltipPosition({
-            x: rect.left + window.scrollX,
-            y:  window.scrollY + 500
-        });
-    };
-
-    const handleMouseLeave = () => {
-        setTooltipData(null);
-    };
-
     return (
         <section className={styles.moviesContainer}>
             <div className={styles.heading}>
@@ -121,10 +75,7 @@ const TrendingList = (props) => {
                     {data.map((item) => (
                         <div
                             className={styles.box}
-                            key={item.id}
-                            onMouseEnter={(e) => handleMouseEnter(item, e)}
-                            onMouseLeave={handleMouseLeave}
-                        >
+                            key={item.id}>
                             <img src={item.image_url} alt={item.title} />
                             <h5>{item.title} - {item.id}</h5>
                             <div className={styles.metaBox}>
@@ -137,10 +88,6 @@ const TrendingList = (props) => {
                         </div>
                     ))}
                 </Slider>
-
-                {tooltipData && (
-                    <Tooltip tooltipData={tooltipData} position={tooltipPosition} />
-                )}
             </div>
         </section>
     );
