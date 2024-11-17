27ad9e9ad54645bd5ff0b9e0c2309380eda19c22
Komal <91592874+DynaminKomal@users.noreply.github.com>
2024-11-17 19:41:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const fs = require('fs');
 const cors = require('cors');
+const morgan = require('morgan')
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan('tiny'))
+}
 
 app.use(cors({
     origin: '*', // Allow this origin
-  }));
+}));
 
 const bannerData = JSON.parse(fs.readFileSync(`${__dirname}/Data/banner.json`, 'utf-8'));
 
