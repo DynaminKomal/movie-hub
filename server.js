d012790882da8df865f8e9d02e14b0dc41f08d6f
Komal <110466516+PrjKomal@users.noreply.github.com>
2024-11-18 10:27:25 +0530

    set up database connection

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 // Load environment variables
 dotenv.config({ path: './config.env' });
 
-const app = require('./app')
+const app = require('./app');
+
+// Database connection
+const connectToDatabase = async () => {
+    try {
+        const db = process.env.DATABASE_STRING
+            .replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+            .replace('DB_NAME', process.env.DATABASE_NAME);
+        await mongoose.connect(db);
+        console.log(`App has been connected to the ${process.env.DATABASE_NAME} database`);
+    } catch (error) {
+        console.error('Database connection error:', error);
+        process.exit(1);
+    }
+};
 
 
 // Start the application
 const startApp = async () => {
+    await connectToDatabase();
     const port = process.env.PORT || 8005;
     app.listen(port, () => {
         console.log(`App running on port: ${port}`);
